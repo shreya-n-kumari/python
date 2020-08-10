ab8d2e36a78bade66372b490fa4e822423bbc92c
shreya-n-kumari <68438422+shreya-n-kumari@users.noreply.github.com>
2020-08-10 23:50:29 +0530

    loop.py

---
+++ loop.py
@@ -0,0 +1,32 @@
+#while loop....
+i = 1
+while i < 6 :
+	print(i)
+	i += 1  #NO SPACE BETWEEN + AND =
+print("\n")
+
+
+i = 1
+while i < 6:
+  print(i)
+  print("\n")
+  if i == 3:
+     break		#break statement stop the loop when i equal to 3
+  i += 1
+  
+i = 0
+while i < 6:
+  i += 1 
+  if i == 3:
+    continue	#continue to the next iteration when i = 3
+  print(i)
+
+	
+
+  #print message once the condition false
+  i = 1
+  while i < 6:
+  	print(i)
+  	i+=1
+  else :
+  	print("\ni is no longer less than 6")
