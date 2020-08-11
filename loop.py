3b1aee55064b13a375a44b413a65427cd46ac361
shreya-n-kumari <68438422+shreya-n-kumari@users.noreply.github.com>
2020-08-11 23:52:23 +0530

    updated loop

--- loop.py
+++ loop.py
@@ -30,3 +30,55 @@ while i < 6:
   	i+=1
   else :
   	print("\ni is no longer less than 6")
+#while loop....
+i = 1
+while i < 6 :
+	print(i)
+	i += 1  #NO SPACE BETWEEN + AND =
+print("\n")
+
+
+i = 1
+while i < 6:
+  print(i)
+  print("\n")
+  if i == 3:
+     break		#break statement stop the loop when i equal to 3
+  i += 1
+  
+"""i = 0
+while i < 6:
+  i += 1 
+  if i == 3: code isn't working
+    continue	#continue to the next iteration when i = 3
+  print(i)"""
+
+	
+
+  #print message once the condition false
+i = 1
+while i < 6:
+  	print(i)
+  	i+=1
+else:
+  print("i is no longer less than 6")
+  print("\n")
+
+#for loop in python.........
+fruits = ["apple", "banana", "cherry"]    #list are written in square bracket.
+for x in fruits:
+	print(x)
+
+for x in "banana":
+	print(x)
+
+#break in for loop.
+for x in fruits:
+	print(x)
+	if x == "banana":
+		break
+
+for x in fruits:
+	if x == "banana":
+		break
+		print(x)
